ed0d3cae6b7851365f4f5d433cbe40b1686cd24d
Mert Celik <otuz.bes.bucuk@hotmail.com>
2021-04-13 15:00:05 +0300

    Update Package.swift to 4.8.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Veriff",
-                      url: "https://cdn.veriff.me/ios/com/veriff/veriff-xcframework/4.7.0/veriff-xcframework-4.7.0.zip",
-                      checksum: "f05b01c07b39f2926245f8d831c598ab7ce2b60aaf4274e776349f42e480c707"),
+                      url: "https://cdn.veriff.me/ios/com/veriff/veriff-xcframework/4.8.0/veriff-xcframework-4.8.0.zip",
+                      checksum: "71b07b631a5153b4ea11ab0d60c17cd9707451c2ec0b3aa8cdb8bd1959d913b9"),
     ]
 )
