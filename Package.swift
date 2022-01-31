642b46bec18990e2605426e497682432891cd5d2
Andrei Iutkin <andrei.iutkin@veriff.net>
2022-01-31 11:45:45 +0000

    Update Package.swift to 4.27.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Veriff",
-                      url: "https://cdn.veriff.me/ios/com/veriff/veriff-xcframework/4.26.0/veriff-xcframework-4.26.0.zip",
-                      checksum: "2eed1c446cfa26b406c47f296097391210295e3dfd8d9b7eacca29072db94301"),
+                      url: "https://cdn.veriff.me/ios/com/veriff/veriff-xcframework/4.27.0/veriff-xcframework-4.27.0.zip",
+                      checksum: "a5f5c1384a29ce5be4c83abf2c8eade0a368ef72fa728a33381edf840ff14394"),
     ]
 )
