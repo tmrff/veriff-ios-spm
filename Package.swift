741bcaacef708d2055adfb8093002840cf2eaaa3
Mert <otuz.bes.bucuk@hotmail.com>
2022-07-19 10:49:18 +0300

    Fixed the path for target

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Veriff",
-                      url: "https://cdn.veriff.me/ios/com/veriff/veriff-xcframework/4.40.0/veriff-xcframework-4.40.0.zip",
+                      url: "https://cdn.veriff.me/ios/com/veriff/veriffsdk/4.40.0/veriffsdk-4.40.0.zip",
                       checksum: "7dfaa70d2229cf4c85af7123cb59645442efde930b3e0569edb899b0e79da227"),
     ]
 )
