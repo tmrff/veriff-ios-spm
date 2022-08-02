5182768a73f8a3d2bedde63ea1cd8ef2933fe673
Andrei Iutkin <andrei.iutkin@veriff.net>
2022-08-02 11:24:10 +0000

    Update Package.swift to 4.41.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(name: "Veriff",
-                      url: "https://cdn.veriff.me/ios/com/veriff/veriffsdk/4.40.0/veriffsdk-4.40.0.zip",
-                      checksum: "7dfaa70d2229cf4c85af7123cb59645442efde930b3e0569edb899b0e79da227"),
+                      url: "https://cdn.veriff.me/ios/com/veriff/veriffsdk/4.41.0/veriffsdk-4.41.0.zip",
+                      checksum: "834db948e933f61183080443990425cb7f8064cc8c421bb0801d39373e576361"),
     ]
 )
